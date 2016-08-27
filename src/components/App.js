8cdbbe7cf4e0c7fc4146c871ea76363b77ef3a26
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-08-27 13:59:25 +0200

    Add a padding around the layout for bigger screens

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import { StyleSheet, css } from 'aphrodite';
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { grey200 } from 'material-ui/styles/colors'
 
 import RouteList from './RouteList'
 import data from './../routes.json'
+import { medium } from './../breakpoints'
+
+const styles = StyleSheet.create({
+  container: {
+    [`@media (min-width: ${medium})`]: {
+      padding: '1rem',
+    }
+  }
+});
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <div style={{backgroundColor: grey200}}>
+        <div style={{backgroundColor: grey200, flex: '1'}} className={css(styles.container)}>
           <div style={{maxWidth: '900px', margin: '0 auto'}}>
             <RouteList routes={data.routes} />
           </div>
