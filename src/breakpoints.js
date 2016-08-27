bdf126a3285e77219680dc661c8d671faa1c2c22
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-08-27 13:57:03 +0200

    Add CSS breakpoints constants

---
+++ src/breakpoints.js
@@ -0,0 +1,6 @@
+// https://material.google.com/layout/responsive-ui.html#responsive-ui-breakpoints
+export const xsmall = "480px"
+export const small = "600px"
+export const medium = "960px"
+export const large = "1280px"
+export const xlarge = "1920px"
