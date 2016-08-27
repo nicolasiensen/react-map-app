11455e91a5192e2c5b40a1b076356dd2ed4aeb32
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-08-27 14:10:25 +0200

    Add support to select a route in the RouteList

--- src/components/RouteList.js
+++ src/components/RouteList.js
@@ -5,12 +5,21 @@ import Paper from 'material-ui/Paper';
 import RouteItem from './RouteItem';
 
 class RouteList extends Component {
+  constructor (props) {
+    super(props)
+    this.selectRoute = this.selectRoute.bind(this)
+  }
+
+  selectRoute (route) {
+    this.props.onRouteSelect(route)
+  }
+
   render() {
     return (
       <Paper>
         <List>
           {
-            this.props.routes.map((route, i) => <RouteItem route={route} key={i} />)
+            this.props.routes.map((route, i) => <RouteItem route={route} onTouchTap={this.selectRoute} key={i} />)
           }
         </List>
       </Paper>
