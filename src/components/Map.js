3c2da6ff563e78083577b7dcf5895b22d1dfa911
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-08-28 09:46:03 +0200

    Add Map component to render a Google Maps embed with routes

---
+++ src/components/Map.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+  componentDidMount() {
+    // TODO: add Google Maps API as a NPM package dependency, instead of loading it in index.html
+    let map = new window.google.maps.Map(document.getElementById(this.props.id), {disableDefaultUI: true});
+    let bounds = new window.google.maps.LatLngBounds();
+
+    this.props.segments.forEach((segment) => {
+      let path = []
+
+      // Generate path based on polyline if it exists
+      if (segment.polyline) {
+        path = window.google.maps.geometry.encoding
+          .decodePath(segment.polyline)
+          .map(l => ({lat: l.lat(), lng: l.lng()}))
+      // Use stops to generate the path if there is no polyline
+      } else {
+        path = segment.stops.map(s => ({lat: s.lat, lng: s.lng}))
+      }
+
+      // Centralize the map view
+      path.forEach((latLng) => { bounds.extend(latLng) })
+
+      const polyline = new window.google.maps.Polyline({
+        path: path,
+        geodesic: true,
+        strokeColor: segment.color,
+        strokeOpacity: 1.0,
+        strokeWeight: 5
+      });
+
+      polyline.setMap(map);
+    })
+
+    map.fitBounds(bounds);
+  }
+
+  render() {
+    return (
+      <div id={this.props.id} style={this.props.style}></div>
+    )
+  }
+}
+
+export default Map;
